667ce7e2f2697f99bd738daab0114c75ed144e85
iamasaqlain-q <109796536+iamasaqlain-q@users.noreply.github.com>
2022-08-01 16:07:17 +0530

    Add files via upload

---
+++ If_Else_Prob1/sumnavg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sum= ($a + $b + $c + $d + $e)
+avg= ($sum / 5)
+prod= ($a * $b * $c * $d * $e)
+
+echo "The sum of these numbers is: " $sum
+echo "The average of these numbers is: " $avg
+echo "The product of these numbers is: " $prod
